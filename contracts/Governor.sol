0ceddcd52f954399c8e8e160257cbfd58e1db354
maggo <me@mar.co.de>
2022-04-23 18:02:06 +0200

    Add new Governor functionality

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts@4.5.0/governance/Governor.sol";
-import "@openzeppelin/contracts@4.5.0/governance/extensions/GovernorSettings.sol";
-import "@openzeppelin/contracts@4.5.0/governance/extensions/GovernorCountingSimple.sol";
-import "@openzeppelin/contracts@4.5.0/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts@4.5.0/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "./interfaces/ILensHub.sol";
+import "./interfaces/IFollowNFT.sol";
 
-contract MyGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotesQuorumFraction {
-    constructor(IVotes _token)
+contract MyGovernor is Governor, GovernorSettings, GovernorCountingSimple {
+    ILensHub public lensHub;
+
+    constructor(ILensHub _lensHub)
         Governor("MyGovernor")
-        GovernorSettings(1 /* 1 block */, 273 /* 1 hour */, 0)
-        GovernorVotesQuorumFraction(4)
-    {}
+        GovernorSettings(
+            1, /* 1 block */
+            273, /* 1 hour */
+            0
+        )
+    {
+        lensHub = _lensHub;
+    }
 
     // The following functions are overrides required by Solidity.
 
@@ -37,19 +46,37 @@ contract MyGovernor is Governor, GovernorSettings, GovernorCountingSimple, Gover
     function quorum(uint256 blockNumber)
         public
         view
-        override(IGovernor, GovernorVotesQuorumFraction)
+        override
         returns (uint256)
     {
-        return super.quorum(blockNumber);
+        uint256 profileId = lensHub.defaultProfile(address(this));
+        require(profileId != 0, "No default profile set");
+
+        address followNFTAddress = lensHub.getFollowNFT(profileId);
+        require(followNFTAddress != address(0), "No followers yet");
+
+        IFollowNFT followNFT = IFollowNFT(followNFTAddress);
+
+        return
+            (followNFT.getDelegatedSupplyByBlockNumber(blockNumber) * 4) / 100;
     }
 
+    // Get Voting Power from Lens
     function getVotes(address account, uint256 blockNumber)
         public
         view
-        override(IGovernor, GovernorVotes)
+        override
         returns (uint256)
     {
-        return super.getVotes(account, blockNumber);
+        uint256 profileId = lensHub.defaultProfile(address(this));
+        require(profileId != 0, "No default profile set");
+
+        address followNFTAddress = lensHub.getFollowNFT(profileId);
+        require(followNFTAddress != address(0), "No followers yet");
+
+        IFollowNFT followNFT = IFollowNFT(followNFTAddress);
+
+        return followNFT.getPowerByBlockNumber(account, blockNumber);
     }
 
     function proposalThreshold()
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,23 @@
-import '@nomiclabs/hardhat-waffle';
+import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
-import '@typechain/hardhat';
-import "solidity-coverage"
+import "@typechain/hardhat";
+import "solidity-coverage";
 import "hardhat-prettier";
-import "hardhat-contract-sizer"
-import "hardhat-gas-reporter"
+import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
 
-import * as fs from 'fs';
-import * as dotenv from 'dotenv'
+import * as fs from "fs";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const mnemonic = fs.existsSync('.secret')
-  ? fs
-      .readFileSync('.secret')
-      .toString()
-      .trim()
-  : "test test test test test test test test test test test junk"
+const mnemonic = fs.existsSync(".secret")
+  ? fs.readFileSync(".secret").toString().trim()
+  : "test test test test test test test test test test test junk";
 
-const infuraKey = process.env.INFURA_KEY
-const etherscanKey = process.env.ETHERSCAN_KEY
-const polyscanKey = process.env.POLYGONSCAN_KEY
+const infuraKey = process.env.INFURA_KEY;
+const etherscanKey = process.env.ETHERSCAN_KEY;
+const polyscanKey = process.env.POLYGONSCAN_KEY;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -30,7 +27,7 @@ export default {
     hardhat: {
       forking: {
         url: `https://mainnet.infura.io/v3/${infuraKey}`,
-        enabled: process.env.FORK === 'true'
+        enabled: process.env.FORK === "true",
       },
       // gas: 12000000,
       // blockGasLimit: 0x1fffffffffffff,
@@ -69,28 +66,27 @@ export default {
       },
     },
   },
-  solidity: '0.8.4',
+  solidity: "0.8.10",
   settings: {
     optimizer: {
       enabled: true,
-      runs: 1
-    }
+      runs: 1,
+    },
   },
   typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
+    outDir: "typechain",
+    target: "ethers-v5",
   },
   contractSizer: {
     alphaSort: true,
   },
   etherscan: {
     // remove comment for polygon ecosystem ( testnet, mainnet, etc)
-    apiKey: polyscanKey
+    apiKey: polyscanKey,
     // remove comment polygon ecosystem ( testnet, mainnet, etc)
     // apiKey: etherscanKey
-
   },
   mocha: {
-    timeout: 150000
-  }
+    timeout: 150000,
+  },
 };
