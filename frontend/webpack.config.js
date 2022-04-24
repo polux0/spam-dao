f2c5bb4a9114c2da73ef323303f0ea6ee265b9f3
maggo <me@mar.co.de>
2022-04-24 04:14:22 +0200

    Fix frontend build issues

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
+      template: "./public/index.html",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
+        use: ["file-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.jsx', '.tsx', '.js'],
+    extensions: [".jsx", ".tsx", ".js"],
   },
-  devtool: 'source-map',
-  mode: "production"
+  devtool: "source-map",
+  mode: "production",
 };
